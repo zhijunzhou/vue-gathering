440e6b1d37991f7627e37426769905cc9d8a9ddb
zhijunzhou <2050588049@qq.com>
2018-06-25 19:33:41 +0800

    增加服务评价模块

--- src/router/index.js
+++ src/router/index.js
@@ -5,20 +5,31 @@ Vue.use(Router)
 
 import Layout from '../views/layout/Layout'
 import Home from '../views/home/Home'
+import Opinion from '../views/opinion/opinion'
 
 export const constantRouterMap = [{
   path: '/',
   name: '/',
   component: Layout,
   redirect: '/home',
-  children: [{
-    path: '/home',
-    name: 'Home',
-    component: Home,
-    meta: {
-      title: '咨询卡'
+  children: [
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      meta: {
+        title: '咨询卡'
+      }
+    },
+    {
+      path: '/opinion',
+      name: 'Opinion',
+      component: Opinion,
+      meta: {
+        title: '服务评价'
+      }
     }
-  }]
+  ]
 }]
 
 export default new Router({
---
+++ src/views/opinion/opinion.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="opinion-container">
+    <div>尚德国贸</div>
+  </div>
+</template>
+
+<style>
+
+</style>
+
