282644626c447f272fdf4ad68b9ac7149af60bca
Zhou <zhijunzhou@users.noreply.github.com>
2018-12-22 13:21:37 +0800

    init

--- src/utils/ServicesConfig.js
+++
@@ -1,9 +0,0 @@
-const ServicesConfig = {
-  cloudServices: {
-    tokenUrl: 'http://localhost:5000/api/token',
-    uploadUrl: 'http://localhost:5000/api/upload',
-    documentId: 'attachment'
-  }
-}
-
-export default ServicesConfig
