90553da3d13adf8b20a2d24b757f389c92c841a2
Zhou <zhijunzhou@users.noreply.github.com>
2018-12-23 20:34:25 +0800

    background size

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" :style="{'background-image': 'url(' + require('./asset/letter/bj1.png') +')'}">
+  <div id="app" :style="{'background-image': 'url(' + require('./asset/letter/bj1.png') +')', 'background-size': '100% 100%'}">
     <router-view></router-view>
   </div>
 </template>
