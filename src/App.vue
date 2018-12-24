9c79c0841d5901926f6034f4839c88c3057238e0
Zhou <zhijunzhou@users.noreply.github.com>
2018-12-24 13:18:33 +0800

    分享页

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div id="app" :style="{'background-image': 'url(' + require('./asset/letter/bj1.png') +')', 'background-size': '100% 100%'}">
+  <div id="app" :style="{'background-image': 'url(' + backgroundImage +')', 'background-size': '100% 100%'}" :class="{'no-padding': nopadding}">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import bj1 from './asset/letter/bj1.png'
+// import bj2 from './asset/letter/bj2.png'
+
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+    return {
+      backgroundImage: bj1,
+      nopadding: false
+    }
+  },
+  created() {
+    if (typeof this.$route.path === 'string' && this.$route.path.indexOf('share') !== -1) {
+      this.backgroundImage = ''
+      this.nopadding = true
+    }
+  }
 }
 </script>
--- src/views/share/share.vue
+++ src/views/share/share.vue
@@ -1,12 +1,81 @@
 <template>
-  <div>分享</div>
+  <div class="share-container">
+    <div class="yahei" v-show="showYahei">
+      <img src="../../asset/letter/line.png" alt="" width="35%" class="fx_zx" />
+      <img src="../../asset/letter/fx.png" alt="" width="70%" class="fx_wz" />
+      <img src="../../asset/letter/zhid.png" @click="hideYahei" alt="" class="zhidao" />
+    </div>
+    <img
+      src="../../asset/letter/7.png"
+      alt=""
+      class="img_bj"
+      id="2411"
+    />
+    <!-- <div class="changan">
+      <img src="../../asset/letter/can_03.png" alt="" width="50%" />
+    </div> -->
+    <!-- </div> -->
+    <!-- <a onclick="swap_music()" class="yinyue">
+      <img src="../../asset/letter/yinfu.png" alt="" width="70%" />
+      <img src="../../asset/letter/open.png" alt="" class="open" width="100%" />
+      <img src="../../asset/letter/close.png" alt="" class="close" width="30%" />
+    </a>
+    <audio
+      id="myaudio"
+      controls="controls"
+      height="100"
+      width="100"
+      hidden="hidden"
+      autoplay="autoplay"
+      loop="loop"
+    >
+      <source src="../../asset/letter/2.mp3" type="audio/mp3" />
+      <source src="../../asset/letter/2.mp3" type="audio/ogg" />
+      <embed height="100" width="100" src="../../asset/letter/2.mp3" />
+    </audio> -->
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      showYahei: true
+    }
+  },
+  methods: {
+    hideYahei() {
+      this.showYahei = false
+    }
   }
 }
 </script>
+
+<style>
+.share-container {
+  position: relative;
+  height: 100%;
+}
+.yahei {
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  text-align: center;
+  position: fixed;
+  top: 0;
+  z-index: 9999;
+}
+.fx_zx {
+  position: absolute;
+  top: 2%;
+  right: 5%;
+}
+.fx_wz {
+  margin-top: 60%;
+}
+.zhidao {
+    margin-top: 20%;
+}
+</style>
+
 
