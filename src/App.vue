1aefa8ba6c418803098a79fdde510cec1e8adaee
Zhou <zhijunzhou@users.noreply.github.com>
2018-12-25 11:14:02 +0800

    切换背景

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,17 @@ export default {
       nopadding: false
     };
   },
+  watch: {
+    $route(to, from) {
+      if (
+        typeof this.$route.path === "string" &&
+        this.$route.path.indexOf("share") !== -1
+      ) {
+        this.backgroundImage = bj2;
+        this.nopadding = true;
+      }
+    }
+  },
   created() {
     if (
       typeof this.$route.path === "string" &&
@@ -39,7 +50,7 @@ export default {
         perfData = window.performance.timing, // The PerformanceTiming interface represents timing-related performance information for the given page.
         EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
         time = parseInt((EstimatedTime / 1000) % 60) * 100;
-  
+
       // Loadbar Animation
       $(".loadbar").animate(
         {
@@ -47,7 +58,7 @@ export default {
         },
         time
       );
-  
+
       // Loadbar Glow Animation
       $(".glow").animate(
         {
@@ -55,21 +66,21 @@ export default {
         },
         time
       );
-  
+
       // Percentage Increment Animation
       var PercentageID = $("#precent"),
         start = 0,
         end = 100,
         durataion = time;
       animateValue(PercentageID, start, end, durataion);
-  
+
       function animateValue(id, start, end, duration) {
         var range = end - start,
           current = start,
           increment = end > start ? 1 : -1,
           stepTime = Math.abs(Math.floor(duration / range)),
           obj = $(id);
-  
+
         var timer = setInterval(function() {
           current += increment;
           $(obj).text(current + "%");
@@ -79,12 +90,12 @@ export default {
           }
         }, stepTime);
       }
-  
+
       // Fading Out Loadbar on Finised
       setTimeout(function() {
         $(".preloader-wrap").fadeOut(300);
       }, time);
-    })
+    });
   }
 };
 </script>
